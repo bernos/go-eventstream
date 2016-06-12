6a174eb97d687d10a37af26de8d8a520405df14b
Brendan McMahon <bernos@gmail.com>
2016-06-12 22:52:54 +1000

    Added test for FromSlice

--- eventstream/stream_test.go
+++ eventstream/stream_test.go
@@ -21,3 +21,21 @@ func TestOnce(t *testing.T) {
 	}
 
 }
+
+func TestFromSlice(t *testing.T) {
+	s := FromSlice([]interface{}{1, 2, 3, 4, 5})
+
+	count := 0
+
+	for e := range s.Events() {
+		count++
+
+		if e.Value().(int) != count {
+			t.Errorf("Want %d, got %d", count, e.Value().(int))
+		}
+	}
+
+	if count != 5 {
+		t.Errorf("Want 5 values, got %d", count)
+	}
+}
