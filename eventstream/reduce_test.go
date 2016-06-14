b86d946cfbf4545029f3faac4b2dca3bf45d03e6
Brendan McMahon <bernos@gmail.com>
2016-06-14 22:24:01 +1000

    Implemented reduce

---
+++ eventstream/reduce_test.go
@@ -0,0 +1,34 @@
+package eventstream
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReduce(t *testing.T) {
+	var (
+		received int
+		count    int
+		input    = FromSlice([]interface{}{1, 2, 3, 4, 5})
+		expect   = 15
+	)
+
+	fn := ReducerFunc(func(acc, value interface{}) (interface{}, error) {
+		return acc.(int) + value.(int), nil
+	})
+
+	out := Reduce(fn).Transform(input)
+
+	for event := range out.Events() {
+		count++
+		received = event.Value().(int)
+	}
+
+	if count != 1 {
+		t.Errorf("Expected 1 value, go %d", count)
+	}
+
+	if !reflect.DeepEqual(received, expect) {
+		t.Errorf("Want %v, got %v", expect, received)
+	}
+}
