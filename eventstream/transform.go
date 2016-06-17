78235909c785fd0da8cda288ceec74669d8f7c68
Brendan McMahon <bernos@gmail.com>
2016-06-17 14:17:26 +1000

    Added chainable API to Transformer interface

--- eventstream/transform.go
+++ eventstream/transform.go
@@ -3,12 +3,67 @@ package eventstream
 type Transformer interface {
 	Transform(Stream) Stream
 	Compose(Transformer) Transformer
+	Filter(Predicate) Transformer
+	FlatMap(FlatMapper) Transformer
+	PFlatMap(FlatMapper, int) Transformer
+	Last() Transformer
+	Map(Mapper) Transformer
+	PMap(Mapper, int) Transformer
+	Reduce(Reducer) Transformer
+	Scan(Reducer) Transformer
+	Take(int) Transformer
+	TakeWhile(Predicate) Transformer
+	TakeUntil(Predicate) Transformer
 }
 
 type TransformerFunc func(Stream) Stream
 
 func (t TransformerFunc) Transform(s Stream) Stream {
 	return t(s)
+}
+
+func (t TransformerFunc) Filter(fn Predicate) Transformer {
+	return t.Compose(Filter(fn))
+}
+
+func (t TransformerFunc) FlatMap(m FlatMapper) Transformer {
+	return t.Compose(FlatMap(m))
+}
+
+func (t TransformerFunc) Last() Transformer {
+	return t.Compose(Last())
+}
+
+func (t TransformerFunc) PFlatMap(m FlatMapper, n int) Transformer {
+	return t.Compose(PFlatMap(m, n))
+}
+
+func (t TransformerFunc) Map(m Mapper) Transformer {
+	return t.Compose(Map(m))
+}
+
+func (t TransformerFunc) PMap(m Mapper, n int) Transformer {
+	return t.Compose(PMap(m, n))
+}
+
+func (t TransformerFunc) Reduce(r Reducer) Transformer {
+	return t.Compose(Reduce(r))
+}
+
+func (t TransformerFunc) Scan(r Reducer) Transformer {
+	return t.Compose(Scan(r))
+}
+
+func (t TransformerFunc) Take(n int) Transformer {
+	return t.Compose(Take(n))
+}
+
+func (t TransformerFunc) TakeUntil(fn Predicate) Transformer {
+	return t.Compose(TakeUntil(fn))
+}
+
+func (t TransformerFunc) TakeWhile(fn Predicate) Transformer {
+	return t.Compose(TakeWhile(fn))
 }
 
 func (t TransformerFunc) Compose(next Transformer) Transformer {
