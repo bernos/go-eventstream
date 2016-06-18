6250df6032c6b023494fa73b4f4a41b2a791c30a
Brendan McMahon <bernos@gmail.com>
2016-06-18 12:57:16 +1000

    Implement fast binary exponential backoff
    Using bitwise operators for retry delay calculation is much faster than
    the math lib, and safer wrt overflows when dealing with long running
    retry periods. Included some benchmarks for comparison

--- eventstream/retry/retry.go
+++ eventstream/retry/retry.go
@@ -63,7 +63,7 @@ func Retry(fn func() (interface{}, error), options ...func(*Retrier)) func() (in
 	}
 
 	return func() (interface{}, error) {
-		count := 0
+		var count int
 
 		for {
 			value, err := fn()
@@ -80,13 +80,40 @@ func Retry(fn func() (interface{}, error), options ...func(*Retrier)) func() (in
 				return nil, fmt.Errorf("Retrier exceeded max retry count of %d. Cause: %s", r.MaxRetries, err.Error())
 			}
 
-			time.Sleep(calculateDelay(count, r.BaseDelay, r.MaxDelay))
+			time.Sleep(calculateDelayBinary(uint(count), r.BaseDelay, r.MaxDelay))
 			count++
 		}
 	}
 }
 
-func calculateDelay(iteration int, baseDelay, maxDelay time.Duration) time.Duration {
+func binaryRaise(exponent uint) int64 {
+	// Clamp exponent to avoid 64 bit overflow
+	if exponent > 62 {
+		exponent = 62
+	}
+
+	return 1 << exponent
+}
+
+func calculateDelayBinary(iteration uint, baseDelay, maxDelay time.Duration) time.Duration {
+	m := (binaryRaise(iteration) - 1) >> 1
+
+	// If multiplier is greater than maxDelay, then we we don't need to
+	// calculate
+	if m > int64(maxDelay) {
+		return maxDelay
+	}
+
+	d := (time.Duration(m) * baseDelay) + baseDelay
+
+	if d < maxDelay && d > 0 {
+		return d
+	}
+
+	return maxDelay
+}
+
+func calculateDelay(iteration uint, baseDelay, maxDelay time.Duration) time.Duration {
 	m := ((math.Pow(2, float64(iteration))) - 1) / 2
 
 	// Check for wrapping, or multiplier greater than max duration in
--- eventstream/retry/retry_test.go
+++ eventstream/retry/retry_test.go
@@ -2,27 +2,62 @@ package retry
 
 import (
 	"fmt"
+	"math"
 	"testing"
 	"testing/quick"
 	"time"
 )
 
-func SkipTestCalculateDelay(t *testing.T) {
-	f := func(max int) bool {
+func TestCalculateDelayBinary(t *testing.T) {
+	maxInt := int((^uint(0)) >> 1)
+
+	tests := []struct {
+		min       int
+		max       int
+		baseDelay time.Duration
+		maxDelay  time.Duration
+	}{
+		{0, 100, time.Millisecond, time.Minute},
+		{maxInt - 10, maxInt + 10, time.Nanosecond, time.Minute},
+	}
+
+	for _, test := range tests {
 		last := time.Duration(0)
 
-		for i := 0; i < max; i++ {
-			d := calculateDelay(i, time.Millisecond, time.Minute)
-			if d < 0 || d < last || d > time.Minute {
-				return false
+		for i := test.min; i < test.max; i++ {
+			d := calculateDelayBinary(uint(i), test.baseDelay, test.maxDelay)
+			if d < 0 || d < last || d > test.maxDelay {
+				t.Errorf("calculateDelayBinary(%d, %s, %s) -> got %d", i, test.baseDelay, test.maxDelay, d)
+			}
+
+			last = d
+		}
+	}
+}
+
+func TestCalculateDelay(t *testing.T) {
+	maxInt := int((^uint(0)) >> 1)
+
+	tests := []struct {
+		min       int
+		max       int
+		baseDelay time.Duration
+		maxDelay  time.Duration
+	}{
+		{0, 100, time.Millisecond, time.Minute},
+		{maxInt - 10, maxInt + 10, time.Nanosecond, time.Minute},
+	}
+
+	for _, test := range tests {
+		last := time.Duration(0)
+
+		for i := test.min; i < test.max; i++ {
+			d := calculateDelay(uint(i), test.baseDelay, test.maxDelay)
+			if d < 0 || d < last || d > test.maxDelay {
+				t.Errorf("calculateDelay(%d, %s, %s) -> got %d", i, test.baseDelay, test.maxDelay, d)
 			}
 			last = d
 		}
-		return true
-	}
-
-	if err := quick.Check(f, nil); err != nil {
-		t.Error(err)
 	}
 }
 
@@ -39,5 +74,43 @@ func TestMaxRetries(t *testing.T) {
 
 	if count != 3 {
 		t.Errorf("Want %d, got %d", 3, count)
+	}
+}
+
+func TestBinaryRaise(t *testing.T) {
+	f := func(x uint) bool {
+		y := binaryRaise(x)
+
+		if y < 0 {
+			return false
+		}
+
+		if x <= 62 {
+			return y == int64(math.Pow(2, float64(x)))
+		} else {
+			return y == int64(math.Pow(2, 62))
+		}
+	}
+
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+var benchmarkBinaryRaiseResult int64
+
+func BenchmarkBinaryRaise(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		benchmarkBinaryRaiseResult = binaryRaise(uint(n))
+	}
+}
+
+var benchmarkMathPowResult int64
+
+func BenchmarkMathPowRaise(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		benchmarkBinaryRaiseResult = int64(math.Pow(2, float64(n)))
 	}
 }
